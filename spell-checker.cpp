ee85ef875424a2d787b2e4412c572e7a84b0c0e7
Amber Gill <amber.bobbilee@gmail.com>
2025-11-14 14:49:48 -0600

    Initial commit - add insert function

---
+++ spell-checker.cpp
@@ -0,0 +1,30 @@
+//Amber Gill
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct TrieNode{
+
+    TrieNode* children[26];
+    bool isEnd;
+
+    TrieNode(){
+        for(int i = 0; i < 26; i++){
+            children[i] = nullptr;}
+        isEnd = false;
+        
+    }
+};
+
+void insert(TrieNode* root, const string& word){
+    TrieNode* node = root;
+
+    for (int i = 0; i < word.size(); i++) {
+        if (node->children[i] == nullptr)
+            node->children[i] = new TrieNode();
+        node = node->children[i];
+    }
+
+    node->isEnd = true;
+}
