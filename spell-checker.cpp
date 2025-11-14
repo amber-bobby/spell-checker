ed745ea9989707b72f99d8d83c305b49adef89c1
Amber Gill <amber.bobbilee@gmail.com>
2025-11-14 15:26:56 -0600

    Initial commit - add delete function & function fixes

--- spell-checker.cpp
+++ spell-checker.cpp
@@ -21,11 +21,11 @@ void insert(TrieNode* root, const string& word){
     TrieNode* node = root;
 
     for (int i = 0; i < word.size(); i++) {
-        if (node->children[i] == nullptr)
-            node->children[i] = new TrieNode();
-        node = node->children[i];
+    int index = word[i] - 'a';
+    if (node->children[index] == nullptr)
+        node->children[index] = new TrieNode();
+    node = node->children[index];
     }
-
     node->isEnd = true;
 }
 
@@ -33,10 +33,69 @@ bool search(TrieNode* root, const string& word) {
     TrieNode* node = root;
 
     for (int i = 0; i < word.size(); i++) {
-        if (node->children[i] == nullptr)
-            return false;
-        node = node->children[i];
+        int index = word[i] - 'a';
+        if (node->children[index] == nullptr){
+            return false;}
+        node = node->children[index];    
     }
-
+    
     return node->isEnd;
 }
+
+bool remove(TrieNode* node, const string& word, int depth = 0) {
+    if (node == nullptr) {
+        return false;
+    }
+
+    if (depth == word.size()) {
+        if (node->isEnd == false) {
+            return false; 
+        }
+
+        node->isEnd = false;
+
+        bool hasChildren = false;
+        for (int i = 0; i < 26; i++) {
+            if (node->children[i] != nullptr) {
+                hasChildren = true;
+                break;
+            }
+        }
+
+        if (hasChildren == false) {
+            return true; 
+        } else {
+            return false; 
+        }
+    }
+
+    
+    int index = word[depth] - 'a';
+    if (node->children[index] == nullptr) {
+        return false; 
+    }
+
+    bool shouldDelete = remove(node->children[index], word, depth + 1);
+
+    if (shouldDelete == true) {
+        delete node->children[index];
+        node->children[index] = nullptr;
+
+        bool hasChildren = false;
+        for (int i = 0; i < 26; i++) {
+            if (node->children[i] != nullptr) {
+                hasChildren = true;
+                break;
+            }
+        }
+
+        if (node->isEnd == false && hasChildren == false) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    return false;
+}
+
